4dc2350f4fd38d5dd8cbab4d3c7a080864802afd
cay <charlottejocelynne@gmail.com>
2025-09-16 18:05:08 +0800

    1st Assignment

---
+++ Go Bank/go.mod
@@ -0,0 +1,3 @@
+module Go_Bank
+
+go 1.25
---
+++ Go Bank/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("˚.🎀༘⋆Welcome to the Go Bank!˚.🎀༘⋆")
+	fmt.Println("What do you want to do?")
+	fmt.Println("1. Balance and Mutation")
+	fmt.Println("2. Deposit")
+	fmt.Println("3. Withdraw")
+	fmt.Println("4. Exit")
+	fmt.Println("Your Choice:")
+	var choice int
+	fmt.Scan(&choice)
+	if choice == 1 {
+	}
+}
