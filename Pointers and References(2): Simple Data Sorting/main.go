6c76aa1b81343733c804378bbe53379677ba61e6
cay <charlottejocelynne@gmail.com>
2025-09-24 21:17:09 +0800

    Pointers and References Part 2

---
+++ Pointers and References(2): Simple Data Sorting/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+)
+
+func sortAsc(arr *[]int) {
+	if len(*arr) == 0 {
+	}
+	for i := 0; i < len(*arr)-1; i++ {
+		for j := i + 1; j < len(*arr); j++ {
+			// save the i-th element into a variable named temp
+			temp := (*arr)[i]
+			if temp > (*arr)[j] {
+				// return the i-th element into temp variable
+				temp = (*arr)[i]
+				// return i-th element to j-th element
+				(*arr)[i] = (*arr)[j]
+				// assign the previous value back to j-th element
+				(*arr)[j] = temp
+			}
+
+		}
+	}
+}
+
+func main() {
+	x := []int{5, 3, 8, 6, 2, -1, 10, 11}
+	xPointers := &x
+	fmt.Println("Original Array:", *xPointers)
+	sortAsc(xPointers)
+	fmt.Println("Sorted Ascending", *xPointers)
+}
